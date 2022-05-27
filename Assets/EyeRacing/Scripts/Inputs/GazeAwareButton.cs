20a7103c9f0b812336f4aac7d0fbb8e6b4a41522
Javier Martínez Albaladejo <jma104>
2022-05-27 13:28:27 +0200

    Rm unused code in GazeAwareButton

--- Assets/EyeRacing/Scripts/Inputs/GazeAwareButton.cs
+++ Assets/EyeRacing/Scripts/Inputs/GazeAwareButton.cs
@@ -24,11 +24,6 @@ public class GazeAwareButton : MonoBehaviour
         originalSize = rectTransform.sizeDelta;
     }
 
-    void changeButtonColor(Color color) {
-        ColorBlock colors = button.colors;
-        colors.normalColor = color;
-        button.colors = colors;
-    }
     void changeSize(float width, float height, float t) {
         Vector2 prev = rectTransform.sizeDelta;
         rectTransform.sizeDelta = new Vector2(Mathf.Lerp(prev.x, width, t*t*t), Mathf.Lerp(prev.y, height, t*t*t));
@@ -40,20 +35,15 @@ public class GazeAwareButton : MonoBehaviour
         if (gazeAware.HasGazeFocus && !isFocused) {
             isFocused = true;
             focusTime = Time.unscaledTime;
-            // Debug.Log("Focused gaze on button " + gameObject.name);
-            // changeButtonColor(new Color(100f, 100f, 255f));
             rectTransform.sizeDelta = originalSize;
         } else if (gazeAware.HasGazeFocus && Time.unscaledTime - focusTime < clickDuration && !alreadyClicked) {
             float t = (Time.unscaledTime - focusTime) / clickDuration;
-            // changeButtonColor(new Color(Mathf.Lerp(255f, 100f, t), Mathf.Lerp(255f, 100f, t), 255f));
             changeSize(originalSize.x * sizePctWhenFullySelected.x, originalSize.y * sizePctWhenFullySelected.y, t);
         } else if (gazeAware.HasGazeFocus && Time.unscaledTime - focusTime >= clickDuration && !alreadyClicked) {
             alreadyClicked = true;
-            // Debug.Log("Gaze-clicked button " + gameObject.name);
             button.onClick.Invoke();
         } else if (!gazeAware.HasGazeFocus) {
             if (isFocused) {
-                // Debug.Log("Released focus from button " + gameObject.name);
                 rectTransform.sizeDelta = originalSize;
             }
             isFocused = false;
